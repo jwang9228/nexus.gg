0064be27c5f181908b8a87c746500814ac14183c
jwang9228 <jwang.srv1@gmail.com>
2025-04-30 21:50:10 -0700

    Add cursor pointer to language buttons

--- src/components/navbar/Languages.jsx
+++ src/components/navbar/Languages.jsx
@@ -9,7 +9,7 @@ export default function Languages() {
     <div className='relative  text-zinc-300/90'>
       <button 
         type='button' 
-        className={`p-2 rounded-t-full ${showRoundedB && 'rounded-b-full'} bg-slate-900`}
+        className={`p-2 rounded-t-full ${showRoundedB && 'rounded-b-full'} bg-slate-900 cursor-pointer`}
         onClick={() => setShowLanguages(!showLanguages)}
       >
         <IoLanguageSharp className='size-[1.13rem] tablet:size-5'/>
@@ -20,7 +20,7 @@ export default function Languages() {
         className={`absolute left-1/2 transform -translate-x-1/2
         transition-all ease-in-out overflow-hidden 
         ${showLanguages ? 'max-h-dvh duration-500 pt-1 pb-1.5' : 'max-h-0 duration-300 py-0'}
-        px-[0.21rem] tablet:px-[0.28rem] rounded-b-full bg-slate-900 text-sm`
+        px-[0.21rem] tablet:px-[0.28rem] rounded-b-full bg-slate-900 text-sm cursor-pointer`
       }>    
         <div className='rounded-full bg-slate-800 px-1 py-0.5'>
           US
